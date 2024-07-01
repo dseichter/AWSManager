48db89259ef9eb9c9f4ce51818e43644b84ac077
Daniel Seichter <daniel.seichter@erpware.de>
2024-07-01 22:11:27 +0200

    #19 Implement functions behind the buttons of the detail page

--- src/ui_aws_ec2.py
+++ src/ui_aws_ec2.py
@@ -6,22 +6,53 @@ import wx.grid
 import aws_ec2
 import settings
 import iconsaws
+import webbrowser
 
 
-# def aws_ec2_load_details( self, event ):
-#     ui_aws_ec2.aws_ec2_load_details(self, event)
+def aws_ec2_refresh_instance(self, event):
+    aws_ec2_load_details(self, event)
 
-# def aws_ec2_reload(self, event):
-#     ui_aws_ec2.aws_ec2_reload(self, event)
 
-# def aws_ec2_refresh_instance( self, event ):
-#     ui_aws_ec2.aws_ec2_refresh_instance(self, event)
+def aws_ec2_open_mgmt_console(self, event):
+    item = self.treeEC2.GetSelection()
+    if item:
+        ec2_instance = aws_ec2.get_ec2_instance(settings.read_config()['region'], self.treeEC2.GetItemText(item).split(' ')[0])
+        url = f"https://eu-central-1.console.aws.amazon.com/ec2/home?region={settings.read_config()['region']}#InstanceDetails:instanceId={ec2_instance['InstanceId']}"
+        webbrowser.open_new_tab(url)
 
-# def aws_ec2_open_mgmt_console( self, event ):
-#     ui_aws_ec2.aws_ec2_open_mgmt_console(self, event)
 
-# def aws_ec2_change_state( self, event ):
-#     ui_aws_ec2.aws_ec2_change_state(self, event)
+def aws_ec2_change_state(self, event):
+    # create a small dialog to ask the user for the new state using a combobox
+    states = ['start', 'stop', 'terminate', 'reboot']
+
+    # remove the 'terminate' option if the instance is already in the 'terminated' state
+    if self.textCtrlEC2_State.GetValue() == 'terminated':
+        states.remove('terminate')
+
+    # remove the 'start' option if the instance is already in the 'running' state
+    if self.textCtrlEC2_State.GetValue() == 'running':
+        states.remove('start')
+
+    # remove the 'stop' option if the instance is already in the 'stopped' state
+    if self.textCtrlEC2_State.GetValue() == 'stopped':
+        states.remove('stop')
+
+    dlg = wx.SingleChoiceDialog(self, 'Select the new state:', 'Change EC2 Instance State', states)
+    if dlg.ShowModal() == wx.ID_OK:
+        selected_state = dlg.GetStringSelection()
+        # perform the necessary actions based on the selected state
+        if selected_state == 'start':
+            aws_ec2.start_ec2_instance(settings.read_config()['region'], self.textCtrlEC2_InstanceId.GetValue())
+        elif selected_state == 'stop':
+            aws_ec2.stop_ec2_instance(settings.read_config()['region'], self.textCtrlEC2_InstanceId.GetValue())
+        elif selected_state == 'terminate':
+            # code to terminate the EC2 instance
+            aws_ec2.terminate_ec2_instance(settings.read_config()['region'], self.textCtrlEC2_InstanceId.GetValue())
+        elif selected_state == 'reboot':
+            # code to reboot the EC2 instance
+            aws_ec2.reboot_ec2_instance(settings.read_config()['region'], self.textCtrlEC2_InstanceId.GetValue())
+    dlg.Destroy()
+
 
 def aws_ec2_load_details(self, event):
     # get the selected item
@@ -47,6 +78,9 @@ def aws_ec2_load_details(self, event):
         self.gridEC2_Volumes.ClearGrid()
         volumes = ec2_instance['BlockDeviceMappings']
         self.gridEC2_Volumes.AppendRows(len(volumes))
+        self.gridEC2_Volumes.SetColLabelSize(18)
+        # add six columns to the grid
+        self.gridEC2_Volumes.AppendCols(6)
         self.gridEC2_Volumes.SetColLabelValue(0, 'Volume ID')
         self.gridEC2_Volumes.SetColLabelValue(1, 'Device')
         self.gridEC2_Volumes.SetColLabelValue(2, 'Type')
@@ -56,11 +90,11 @@ def aws_ec2_load_details(self, event):
         for i, volume in enumerate(volumes):
             volume_information = aws_ec2.get_ec2_volume(settings.read_config()['region'], volume['Ebs'].get('VolumeId', ''))
             self.gridEC2_Volumes.SetCellValue(i, 0, volume_information.get('VolumeId', ''))
-            self.gridEC2_Volumes.SetCellValue(i, 1, volume_information.get('Device', ''))
+            self.gridEC2_Volumes.SetCellValue(i, 1, volume_information['Attachments'][0].get('Device', ''))
             self.gridEC2_Volumes.SetCellValue(i, 2, volume_information.get('VolumeType', ''))
-            self.gridEC2_Volumes.SetCellValue(i, 3, volume_information.get('Size', ''))
-            self.gridEC2_Volumes.SetCellValue(i, 4, volume_information.get('Iops', ''))
-            self.gridEC2_Volumes.SetCellValue(i, 5, volume_information.get('Throughput', '0'))
+            self.gridEC2_Volumes.SetCellValue(i, 3, str(volume_information.get('Size', '0')))
+            self.gridEC2_Volumes.SetCellValue(i, 4, str(volume_information.get('Iops', '0')))
+            self.gridEC2_Volumes.SetCellValue(i, 5, str(volume_information.get('Throughput', '0')))
         self.gridEC2_Volumes.AutoSizeColumns()
 
 
